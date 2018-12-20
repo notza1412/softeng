9822fe1775123db5b2d51dae41894853f7297872
notza1412 <voranathp58@email.nu.ac.th>
2018-12-21 03:40:07 +0700

    first commit

---
+++ src/pages/pay/pay.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MapPage } from '../map/map';
+/**
+ * Generated class for the PayPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-pay',
+  templateUrl: 'pay.html',
+})
+export class PayPage {
+
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PayPage');
+  }
+  goConPage() {
+    this.navCtrl.push(MapPage);
+  }
+
+}
